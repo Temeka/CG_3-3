6bb68c66724813e0b3cb2ab6a1f77938a40381f9
Temeka <misstemeka1@live.com>
2018-07-28 19:58:55 -0500

    Updated

--- CsharpBasicUnderstanding/Program.cs
+++ CsharpBasicUnderstanding/Program.cs
@@ -6,7 +6,33 @@ namespace CsharpBasicUnderstanding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //MY OWN UNDERSTANDING OF C#
+
+
+            //***Statement***: A line of code that concludes with a ";" (semi-colon)
+
+            Console.WriteLine("Win Raymere's Games!");
+            
+            //**Expression**: A conjunction of 1 or more operators and operands
+
+            y + 6 // the "y" and "6" are the operands and the "+" is the operator
+
+            //***Operator***: A symbols that assigns an action... i.e +, *, /, -
+                 
+            144 / 12 // the division sign (/) is the operator
+
+           //***Operand***: Basically they are changeable values in a expression or statement
+
+            x = 5 // the x and 5 are considered operands
+
+           
+
+     
+
+
+
+
+
         }
     }
 }
